Add discriminator loss, correct output normalization

style-based-gan-pytorch/generate_adversarial.py
@@ -3,15 +3,39 @@
 '''
 import argparse
 import math
+import numpy as np
+import random
 
 import torch
 from torchvision import utils
 
-from model import StyledGenerator
+from model import StyledGenerator, Discriminator
 import lpips
 
 from torchattacks import PGD
 import pdb
+
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+
+
+def visualize_data_distribution(z, adv_z):
+    pca = PCA(2)
+
+    z_proj = pca.fit_transform(z)
+    adv_z_proj = pca.fit_transform(adv_z)
+
+    plt.scatter(z_proj[:, 0], z_proj[:, 1], label='Orig', alpha=0.20)
+    plt.scatter(adv_z_proj[:, 0], adv_z_proj[:, 1], label='Adv', alpha=0.20)
+    plt.legend()
+    plt.savefig(f'latent_space_vis.png')
+
+
+def seed_everything(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
 
 
 @torch.no_grad()
@@ -43,48 +67,79 @@ def sample(generator, step, mean_style, n_sample, device):
     return image
 
 
-def attack(generator, step, mean_style, n_sample, device):
+def attack(generator, discriminator, step, mean_style, n_sample, device):
 #     loss_fn = lpips.PerceptualLoss(
 #         model="net-lin", net="vgg", use_gpu=device.startswith("cuda")
 #     )
-    loss_fn = None
+    
+    z_list, adv_z_list = [], []
+    images, adv_images = [], []
+    for i in range(1):
+        z = torch.randn(n_sample, 512).to(device)
+        noise = []
+        for i in range(step + 1):
+            size = 4 * 2 ** i
+            noise.append(torch.randn(z.shape[0], 1, size, size, device=z.device))
 
-    z = torch.randn(n_sample, 512).to(device)
-    noise = []
-    for i in range(step + 1):
-        size = 4 * 2 ** i
-        noise.append(torch.randn(z.shape[0], 1, size, size, device=z.device))
-
-    adversary = PGD(generator, loss_fn, eps=0.05)
-    adv_z = adversary(
-        z,
-        noise,
-        step=step,
-        alpha=1,
-        mean_style=mean_style,
-        style_weight=0.7,
-    )
-
-    print(torch.mean((z-adv_z) **2))
-
-    with torch.no_grad():
-        image = generator(
+        adversary = PGD(generator, discriminator, alpha=1, 
+                        steps=1000, random_start=False, eps=0.05)
+        adv_z = adversary(
             z,
-            noise=noise,
-            step=step,
-            alpha=1,
-            mean_style=mean_style,
-            style_weight=0.7,
-        )
-        adv_image = generator(
-            adv_z,
-            noise=noise,
+            noise,
             step=step,
             alpha=1,
             mean_style=mean_style,
             style_weight=0.7,
         )
 
+        z_list.extend(z)
+        adv_z_list.extend(adv_z)
+
+#         visualize_data_distribution(
+#             z.detach().cpu().numpy(), adv_z.detach().cpu().numpy())
+
+#         mu = torch.mean(z, dim=0)
+#         sigma = torch.std(z, dim=0)
+#         adv_mu = torch.mean(adv_z, dim=0)
+#         adv_sigma = torch.std(adv_z, dim=0)
+#         print(-0.5 * (1. + (sigma **2).log() - mu **2 - sigma **2).mean())
+#         print(-0.5 * (1. + (adv_sigma **2).log() - adv_mu **2 - adv_sigma **2).mean())
+#         print(torch.mean((z-adv_z) **2).item())
+
+        with torch.no_grad():
+            image = generator(
+                z,
+                noise=noise,
+                step=step,
+                alpha=1,
+                mean_style=mean_style,
+                style_weight=0.7,
+            )
+            adv_image = generator(
+                adv_z,
+                noise=noise,
+                step=step,
+                alpha=1,
+                mean_style=mean_style,
+                style_weight=0.7,
+            )
+            images.extend(image)
+            adv_images.extend(adv_image)
+    
+    z = torch.stack(z_list, dim=0)
+    adv_z = torch.stack(adv_z_list, dim=0)
+    visualize_data_distribution(
+        z.detach().cpu().numpy(), adv_z.detach().cpu().numpy())
+
+    mu = torch.mean(z, dim=0)
+    sigma = torch.std(z, dim=0)
+    adv_mu = torch.mean(adv_z, dim=0)
+    adv_sigma = torch.std(adv_z, dim=0)
+    print(-0.5 * (1. + (sigma **2).log() - mu **2 - sigma **2).mean())
+    print(-0.5 * (1. + (adv_sigma **2).log() - adv_mu **2 - adv_sigma **2).mean())
+
+    image = torch.stack(images, dim=0)
+    adv_image = torch.stack(adv_images, dim=0)
     return image, adv_image
     
 
@@ -127,24 +182,38 @@ def style_mixing(generator, step, mean_style, n_source, n_target, device):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--size', type=int, default=1024, help='size of the image')
+    parser.add_argument('--seed', type=int, default=42, help='seed')
     parser.add_argument('--n_row', type=int, default=3, help='number of rows of sample matrix')
     parser.add_argument('--n_col', type=int, default=5, help='number of columns of sample matrix')
+    parser.add_argument(
+        '--no_from_rgb_activate',
+        action='store_true',
+        help='use activate in from_rgb (original implementation)',
+    )
     parser.add_argument('path', type=str, help='path to checkpoint file')
-    
+#     parser.add_argument('disc_path', type=str, help='path to discriminator checkpoint file')
+
     args = parser.parse_args()
     
     device = 'cuda'
+    seed_everything(args.seed)
 
+    ckpt = torch.load(args.path)
     generator = StyledGenerator(512).to(device)
-    generator.load_state_dict(torch.load(args.path)['g_running'])
+    generator.load_state_dict(ckpt['g_running'])
     generator.eval()
+
+    disc_ckpt = ckpt # torch.load(args.path)
+    discriminator = Discriminator(from_rgb_activate=not args.no_from_rgb_activate).to(device)
+    discriminator.load_state_dict(disc_ckpt['discriminator'])
+    discriminator.eval()
 
     mean_style = get_mean_style(generator, device)
 
     step = int(math.log(args.size, 2)) - 2
-    
+
     # img = sample(generator, step, mean_style, args.n_row * args.n_col, device)
-    img, adv_img = attack(generator, step, mean_style, args.n_row * args.n_col, device)
+    img, adv_img = attack(generator, discriminator, step, mean_style, args.n_row * args.n_col, device)
     utils.save_image(img, 'sample.png', nrow=args.n_col, normalize=True, range=(-1, 1))
     utils.save_image(adv_img, 'sample_adv.png', nrow=args.n_col, normalize=True, range=(-1, 1))
     

style-based-gan-pytorch/torchattacks/attacks/pgd.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 from ..attack import Attack
 from ..losses import IDMRFLoss
@@ -29,14 +30,15 @@ class PGD(Attack):
         >>> adv_images = attack(images, labels)
 
     """
-    def __init__(self, model, loss_fn, eps=0.3, alpha=2/255, steps=40, random_start=True):
+    def __init__(self, model, discriminator, eps=0.3, alpha=2/255, steps=40, random_start=True):
         super().__init__("PGD", model)
         self.eps = eps
         self.alpha = alpha
         self.steps = steps
         self.random_start = random_start
         self._supported_mode = ['default', 'targeted']
-        self.loss_fn = IDMRFLoss() # loss_fn # nn.MSELoss()
+        self.discriminator = discriminator
+        self.loss_fn = nn.MSELoss() # IDMRFLoss()
 
     def forward(self, 
                 latent, 
@@ -50,36 +52,54 @@ class PGD(Attack):
         Overridden.
         """
         with torch.no_grad():
-            images = self.model(latent, noise=noise, step=step, alpha=alpha, mean_style=mean_style, 
-                                style_weight=style_weight, mixing_range=mixing_range)
-        images = (images - images.min()) / (images.max() - images.min())
+            images = self.model(
+                latent, noise=noise, step=step, alpha=alpha, mean_style=mean_style, 
+                style_weight=style_weight, mixing_range=mixing_range)
         latent = latent.clone().detach().to(self.device)
-
+        
         adv_latent = latent.clone().detach()
 
         if self.random_start:
             # Starting at a uniformly random point
             adv_latent = adv_latent + torch.empty_like(adv_latent).uniform_(-self.eps, self.eps)
             adv_latent = adv_latent.detach()
-            # adv_latent = torch.clamp(adv_latent, min=0, max=1).detach()
 
         for _ in range(self.steps):
             adv_latent.requires_grad = True
-            outputs = self.model(adv_latent, noise=noise, step=step, alpha=alpha, mean_style=mean_style, 
-                                 style_weight=style_weight, mixing_range=mixing_range)
-            outputs = (outputs - outputs.min()) / (outputs.max() - outputs.min())
+            outputs = self.model(
+                adv_latent, noise=noise, step=step, alpha=alpha, mean_style=mean_style, 
+                style_weight=style_weight, mixing_range=mixing_range)
+
+            # Calculate discriminator loss
+            disc_out = self.discriminator(outputs, step=step, alpha=alpha)
+            loss = F.softplus(-disc_out).mean()
+            print('Disc:', loss.item())
+
+            # drive outputs to zeros, adv attack needs to actually minimize this loss
+            # loss = -self.loss_fn(outputs, torch.zeros_like(outputs)) # .sum()
+
+            # enforce KL divergence for adversarial latents
+            mu = torch.mean(adv_latent, dim=0)
+            sigma = torch.std(adv_latent, dim=0)
+            kl_loss = -0.5 * (1. + (sigma **2).log() - mu **2 - sigma **2).mean()
+            print('KL:', kl_loss.item())
+
+            loss = loss - 100. * kl_loss
+#             print('Wrong predictions: {} / {}'.format(
+#                 (disc_out.sigmoid() < 0.5).sum().item(), disc_out.shape[0]))
 
             # Calculate loss
-            loss = self.loss_fn(outputs, images) # .sum()
-            print(loss)
+            # loss = self.loss_fn(outputs, images) # .sum()
+
+            loss.backward()
+            print('Loss:', loss.item())
 
             # Update adversarial images
-            grad = torch.autograd.grad(loss, adv_latent,
-                                       retain_graph=False, create_graph=False)[0]
-
+            # grad = torch.autograd.grad(loss, adv_latent, create_graph=False)[0]
+            grad = adv_latent.grad
             adv_latent = adv_latent.detach() + self.alpha * grad.sign()
-            delta = torch.clamp(adv_latent - latent, min=-self.eps, max=self.eps)
-            adv_latent = (latent + delta).detach()
-            # adv_latent = torch.clamp(latent + delta, min=0, max=1).detach()
+#             delta = torch.clamp(adv_latent - latent, min=-self.eps, max=self.eps)
+#             adv_latent = (latent + delta).detach()
+            adv_latent = adv_latent.detach()
 
         return adv_latent